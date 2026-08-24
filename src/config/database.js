03991d282ac60f5e371a2020d59c89266d484289
Maksym Gaysha <max.gaysha@gmail.com>
2026-08-24 14:13:41 +0200

    Implement Auth

--- src/config/database.js
+++ src/config/database.js
@@ -3,7 +3,9 @@ import 'dotenv/config';
 import { neon, neonConfig } from '@neondatabase/serverless';
 import { drizzle } from 'drizzle-orm/neon-http';
 
-if (process.env.NODE_ENV === 'development') {
+// if (process.env.NODE_ENV === 'development') {
+// Neon local override is only needed if using a local container proxy (e.g., neon-local)
+if (process.env.NEON_LOCAL === 'true') {
   neonConfig.fetchEndpoint = 'http://neon-local:5432/sql';
   neonConfig.useSecureWebSocket = false;
   neonConfig.poolQueryViaFetch = true;
